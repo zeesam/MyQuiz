99f81010ecf39525aa8cd656cc8c43ba90fd1209
zeesam <govindsamadram@gmail.com>
2020-11-30 20:56:04 +0530

    Added new resources

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,28 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-sm-4">
+                        @include('layouts.sidebar')
+                      </div>
+                      <div class="col-sm-8">
+                        <div class="card">
+                          <h5 class="card-header">Featured</h5>
+                          <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                      </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,6 @@
+<a href="#" class="btn btn-primary btn-lg btn-block">Create Quiz</a>
+<a href="#" class="btn btn-secondary btn-lg btn-block">View Quiz</a>
+<a href="#" class="btn btn-warning btn-lg btn-block">Create Question</a>
+<a href="#" class="btn btn-success btn-lg btn-block">View Question</a>
+<a href="#" class="btn btn-info btn-lg btn-block">Create User</a>
+<a href="#" class="btn btn-secondary btn-lg btn-block">View User</a>
